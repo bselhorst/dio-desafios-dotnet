d2cc1428994743a2fd6018b505aeede67e45fe3b
Bruno Selhorst <bselhorst88@gmail.com>
2024-10-05 11:44:57 -0500

    Desafio Concluído

--- DesafioConsultasRelacionaisSQL/Consultas.sql
+++ DesafioConsultasRelacionaisSQL/Consultas.sql
@@ -30,28 +30,28 @@ SELECT Ano, Count(*) AS Quantidade FROM Filmes GROUP BY Ano ORDER BY Duracao DES
 
 -- 8. Buscar os atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
 -- De acordo com a imagem
-SELECT * FROM Atores WHERE Genero='M'
+SELECT * FROM Atores WHERE Genero='M';
 -- Solução correta conforme o pedido
-SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero='M'
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero='M';
 
 -- 9. Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome e ordenando pelo PrimeiroNome
 -- De acordo com a imagem
-SELECT * FROM Atores WHERE Genero='F' ORDER BY PrimeiroNome
+SELECT * FROM Atores WHERE Genero='F' ORDER BY PrimeiroNome;
 -- Solução correta conforme o pedido
-SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero='F' ORDER BY PrimeiroNome
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero='F' ORDER BY PrimeiroNome;
 
 -- 10. Buscar o nome do filme e o gênero
 SELECT f.Nome AS Nome, g.Genero AS Genero FROM Filmes f 
 INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
-INNER JOIN Generos g ON g.Id = fg.IdGenero 
+INNER JOIN Generos g ON g.Id = fg.IdGenero ;
 
 -- 11. Buscar o nome do filme e o gênero do tipo "Mistério"
 SELECT f.Nome AS Nome, g.Genero AS Genero FROM Filmes f 
 INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
 INNER JOIN Generos g ON g.Id = fg.IdGenero 
-WHERE g.Genero='Mistério'
+WHERE g.Genero='Mistério';
 
 -- 12. Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu papel.
 SELECT f.Nome AS Nome, a.PrimeiroNome AS PrimeiroNome, a.UltimoNome AS UltimoNome, ef.Papel AS Papel FROM Filmes f 
 INNER JOIN ElencoFilme ef ON f.Id = ef.IdFilme
-INNER JOIN Atores a ON a.Id = ef.IdAtor
+INNER JOIN Atores a ON a.Id = ef.IdAtor;
